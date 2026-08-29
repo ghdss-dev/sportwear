b3fc7c297f85c91bcf88e166717328c747469fdb
Gustavo Henrique de Souza Silva <ghdss_ti@hotmail.com>
2026-08-29 13:44:07 -0300

    Project created

---
+++ back-end/src/main/java/br/com/sportwear/SportwearApplication.java
@@ -0,0 +1,13 @@
+package br.com.sportwear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportwearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportwearApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/br/com/sportwear/SportwearApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sportwear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportwearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
